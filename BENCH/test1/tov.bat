ef971eb4c75b85b7fd47eff967ad5ec450d28a21
Fabrizio Tappero <fabrizio.tappero@gmail.com>
2015-06-23 20:36:08 +0200

    added content for project

---
+++ BENCH/test1/tov.bat
@@ -0,0 +1,7 @@
+path;
+path=..\..\ctool
+hexdasm test1.hex
+copy *.mif ..\..\rtl
+copy *.h ..\..\rtl
+copy *.v ..\..\rtl
+copy *.h ..\..\sim
---
+++ RTL/rom_set.h
@@ -0,0 +1,15 @@
+/******************************************************************
+ *                                                                * 
+ *    Author: Liwei                                               * 
+ *                                                                * 
+ *    This file is part of the "ClaiRISC" project,                *
+ *    The folder in CVS is named as "lwrisc"                      * 
+ *    Downloaded from:                                            * 
+ *    http://www.opencores.org/pdownloads.cgi/list/lwrisc         * 
+ *                                                                * 
+ *    If you encountered any problem, please contact me via       * 
+ *    Email:mcupro@opencores.org  or mcupro@163.com               * 
+ *                                                                * 
+ ******************************************************************/
+
+`define    ROM_TYPE  rom128x12
